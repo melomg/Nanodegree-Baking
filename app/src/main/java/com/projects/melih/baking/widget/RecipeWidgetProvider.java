8693aca1a620247b25d64abdde70b378c7d635c1
Melih Gültekin <mmelihgultekin@gmail.com>
2018-05-20 11:52:59 +0300

    feat: adds addIngredients method

--- app/src/main/java/com/projects/melih/baking/widget/RecipeWidgetProvider.java
+++ app/src/main/java/com/projects/melih/baking/widget/RecipeWidgetProvider.java
@@ -3,10 +3,23 @@ package com.projects.melih.baking.widget;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.TextUtils;
+import android.text.style.BulletSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.widget.RemoteViews;
 
 import com.projects.melih.baking.R;
+import com.projects.melih.baking.common.Utils;
+import com.projects.melih.baking.model.Ingredient;
 import com.projects.melih.baking.model.Recipe;
+
+import java.util.List;
 
 /**
  * Implementation of App Widget functionality.
@@ -17,7 +30,8 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_app_widget);
         views.setTextViewText(R.id.appwidget_ingredients_info, recipe.getName());
-        views.setTextViewText(R.id.appwidget_ingredients_header, String.valueOf(recipe.getIngredients()));
+
+        views.setTextViewText(R.id.appwidget_ingredients_header, addIngredients(context, recipe.getIngredients()));
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
@@ -54,5 +68,32 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    private static SpannableStringBuilder addIngredients(@NonNull Context context, @Nullable List<Ingredient> ingredients) {
+        SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder();
+        if (ingredients != null) {
+            int size = ingredients.size();
+            for (int i = 0; i < size; i++) {
+                int startIndex = spannableStringBuilder.length();
+                final Ingredient ingredient = ingredients.get(i);
+                final String ingredientName = ingredient.getName();
+                if (!TextUtils.isEmpty(ingredientName)) {
+                    spannableStringBuilder.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), startIndex, startIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    spannableStringBuilder.append(Utils.getNumberFormatter().format(ingredient.getQuantity()));
+                    final String blank = " ";
+                    spannableStringBuilder.append(blank);
+                    spannableStringBuilder.append(ingredient.getMeasure());
+                    spannableStringBuilder.setSpan(new BulletSpan(10, R.color.black), startIndex, spannableStringBuilder.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    spannableStringBuilder.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.red)), startIndex, spannableStringBuilder.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    spannableStringBuilder.append(blank);
+                    spannableStringBuilder.append(ingredientName);
+                    if (i != size - 1) {
+                        spannableStringBuilder.append("\n");
+                    }
+                }
+            }
+        }
+        return spannableStringBuilder;
     }
 }
